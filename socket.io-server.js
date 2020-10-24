f18ff65fa45ad71cd073bc6d20e4ea420b9d5c20
Abdelraoof Hosam <raoofpx@gmail.com>
2020-10-24 10:47:37 +0200

    Store messages in database

--- socket.io-server.js
+++ socket.io-server.js
@@ -13,7 +13,21 @@ User.init({
   username: { type: DataTypes.STRING, allowNull: false }
 }, {
   sequelize,
-  modelName: 'User'
+  modelName: 'User',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+class Message extends Model {}
+Message.init({
+  user_id: { type: DataTypes.BIGINT, allowNull: false },
+  room: { type: DataTypes.STRING, allowNull: false },
+  message: { type: DataTypes.TEXT, allowNull: false }
+}, {
+  sequelize,
+  modelName: 'Message',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
 });
 
 server.listen(port, () => {
@@ -24,13 +38,21 @@ async function fetchUser(token) {
     return User.findOne({ attributes: ['id', 'username'], where: { token: token } });
 }
 
+async function storeMessage(user_id, room, message) {
+    return Message.create({
+        user_id: user_id,
+        room: room,
+        message: message
+    }, { fields: ['user_id', 'room', 'message'] });
+}
+
 // Register a middleware to authenticate before joining any room
 io.use(async (socket, next) => {
   const user = await fetchUser(socket.handshake.query.token);
   let room = socket.handshake.query.room;
   if (room === 'public-room' || room.split('-').includes(user.id.toString())) {
     // we store username & room in the socket session for this client
-    socket.username = user.username;
+    socket.user = user;
     socket.room = room;
     return next();
   }
@@ -40,10 +62,10 @@ io.use(async (socket, next) => {
 let users = new Set();
 
 io.on('connection', (socket) => {
-  console.log(`Add ${socket.username} to room: ${socket.room}`);
+  console.log(`Add ${socket.user.username} to room: ${socket.room}`);
   socket.join(socket.room);
 
-  users.add(socket.username);
+  users.add(socket.user.username);
 
   // send a list of online users to the client
   socket.emit('login', {
@@ -52,15 +74,18 @@ io.on('connection', (socket) => {
 
   // echo globally (all clients) that a person has connected
   socket.broadcast.emit('user joined', {
-    username: socket.username,
+    username: socket.user.username,
     users: [...users.values()]
   });
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
+  socket.on('new message', async (data) => {
+    // store message in database
+    await storeMessage(socket.user.id, socket.room, data);
+
     // we tell the client to execute 'new message'
     socket.to(socket.room).emit('new message', {
-      username: socket.username,
+      username: socket.user.username,
       message: data
     });
   });
@@ -68,24 +93,24 @@ io.on('connection', (socket) => {
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', () => {
     socket.to(socket.room).emit('typing', {
-      username: socket.username
+      username: socket.user.username
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', () => {
     socket.to(socket.room).emit('stop typing', {
-      username: socket.username
+      username: socket.user.username
     });
   });
 
   // when the user disconnects.. perform this
   socket.on('disconnect', () => {
-    users.delete(socket.username);
+    users.delete(socket.user.username);
 
     // echo globally that this client has left
     socket.broadcast.emit('user left', {
-      username: socket.username,
+      username: socket.user.username,
       users: [...users.values()]
     });
   });
